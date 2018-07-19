0a806dc38216d340b0e51f9a03646a513858dcf5
Hassan Mccutchen <mccutchen.hassan@gmail.com>
2018-07-19 12:48:04 -0400

    this poke version makes api calls and has styling

--- main.js
+++ main.js
@@ -1,15 +1,207 @@
 
-function loadDoc() {
+
   var xhttp = new XMLHttpRequest();
+
+      function display(){
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("data-stuff").innerHTML =
-      this.responseText;
-    }
-  };
-  xhttp.open("GET", "https://pokeapi.co/api/v1/sprite/1/", true);
+
+
+     var r = xhttp.responseText;
+     var x = JSON.parse(r);
+      var abilities = []
+   var name = x.name;
+     abilities = x.abilities;
+     var attack = x.stats[4].base_stat;
+     var defense = x.stats[3].base_stat;
+     var hp = x.stats[5].base_stat;
+
+function createPokemon(abilities,name,attack,defense,hp){
+
+          floreon = {
+          name: name,
+          hp: hp,
+          attack: attack,
+          defense: defense,
+         abilities: abilities
+      }
+
+        abilities.push(abilities);
+     console.log(floreon);
+}
+createPokemon(abilities, name, attack, defense, hp);
+ }
+}
+  }
+
+  xhttp.open("GET", "https://pokeapi.co/api/v2/pokemon/flareon/", true);
+
   xhttp.send();
+
+
+var xhr = new XMLHttpRequest();
+
+      function display2(){
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+
+
+    var r = xhr.responseText;
+    var x = JSON.parse(r);
+
+     var abilities = []
+   var name = x.name;
+     abilities = x.abilities;
+     var attack = x.stats[4].base_stat;
+     var defense = x.stats[3].base_stat;
+     var hp = x.stats[5].base_stat;
+
+function createPokemon(abilities,name,attack,defense,hp){
+
+          vaporeon = {
+          name: name,
+          hp: hp,
+          attack: attack,
+          defense: defense,
+         abilities: abilities
+      }
+
+        abilities.push(abilities);
+     console.log(vaporeon);
+}
+createPokemon(abilities, name, attack, defense, hp);
+ }
+}
+  }
+  xhr.open("GET", "https://pokeapi.co/api/v2/pokemon/vaporeon/", true);
+  xhr.send();
+
+
+
+
+
+var xhp = new XMLHttpRequest();
+
+      function display3(){
+  xhp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+
+    var r = xhp.responseText;
+    var x = JSON.parse(r);
+
+     var abilities = []
+   var name = x.name;
+     abilities = x.abilities;
+     var attack = x.stats[4].base_stat;
+     var defense = x.stats[3].base_stat;
+     var hp = x.stats[5].base_stat;
+
+
+function createPokemon(abilities,name,attack,defense,hp){
+
+          jolteon = {
+          name: name,
+          hp: hp,
+          attack: attack,
+          defense: defense,
+         abilities: abilities
+      }
+
+        abilities.push(abilities);
+     console.log(jolteon);
+}
+createPokemon(abilities, name, attack, defense, hp);
+ }
+}
+  }
+
+  xhp.open("GET", "https://pokeapi.co/api/v2/pokemon/jolteon/", true);
+
+  xhp.send();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//       // for(var i = 0; i < x.abilities.length; i++){
+//     //   console.log(x);
+//       // }
+
+  // var xhr = new XMLHttpRequest();
+  // xhr.open("GET", "https://pokeapi.co/api/v2/ability/62/", true);
+  // xhr.send();
+
+
+    // var r = xhr.responseText;
+    // var h = JSON.parse(r);
+
+     // console.log(h.name);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var images = ["deoxys.png","joltean.png","vaporeon.png"];
+
+function displayAvatar(){
+
+addEventListener("click", function(){
+  count = 0;
+var a = document.getElementById("data-stuff");
+var b = document.createElement("img");
+
+b.src = "images/"+ images[count];
+b.width = "300"
+a.appendChild(b);
+
+count +=1;
+
+})
 
 }
 
 
+
+
+//animations
+  var avatar = document.getElementById("avatar1");
+    avatar1.addEventListener("click", function(){
+   avatar1.classList.toggle("flash");
+
+  })
+
+
+  var avatar2 = document.getElementById("avatar2");
+    avatar2.addEventListener("click", function(){
+   avatar2.classList.toggle("flash");
+
+  })
+
+
+  var avatar3 = document.getElementById("avatar3");
+    avatar3.addEventListener("click", function(){
+   avatar3.classList.toggle("flash");
+
+  })
