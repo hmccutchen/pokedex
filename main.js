dbea4794b9e79a780f7d7c6fbaf975fb8a6179d1
Hassan Mccutchen <mccutchen.hassan@gmail.com>
2018-07-20 01:57:39 -0400

    changes api calls created seperate functions to display pokemon AFTER stroring on objects

--- main.js
+++ main.js
@@ -2,9 +2,10 @@
 
   var xhttp = new XMLHttpRequest();
 
-      function display(){
+
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
+        console.log("getting called");
 
 
      var r = xhttp.responseText;
@@ -16,19 +17,6 @@
      var defense = x.stats[3].base_stat;
      var hp = x.stats[5].base_stat;
 
-
-var i = 0;
-
-typeWriter();
-
-function typeWriter(){
-  if (i < name.length) {
-    document.getElementById("poke-name").innerHTML += name.charAt(i);
-    i++;
-    setTimeout(typeWriter, 100);
-  }
-
-}
 
 
 
@@ -50,19 +38,24 @@ function createPokemon(abilities,name,attack,defense,hp){
 createPokemon(abilities, name, attack, defense, hp);
  }
 }
-  }
+
 
   xhttp.open("GET", "https://pokeapi.co/api/v2/pokemon/flareon/", true);
 
   xhttp.send();
 
 
+
+
+
+
+
 var xhr = new XMLHttpRequest();
 
-      function display2(){
+
   xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-
+console.log("getting called");
 
     var r = xhr.responseText;
     var x = JSON.parse(r);
@@ -74,20 +67,6 @@ var xhr = new XMLHttpRequest();
      var defense = x.stats[3].base_stat;
      var hp = x.stats[5].base_stat;
 
-
-
-     var i = 0;
-
-typeWriter();
-
-function typeWriter(){
-  if (i < name.length) {
-    document.getElementById("poke-name").innerHTML += name.charAt(i);
-    i++;
-    setTimeout(typeWriter, 100);
-  }
-
-}
 
 function createPokemon(abilities,name,attack,defense,hp){
 
@@ -106,7 +85,7 @@ function createPokemon(abilities,name,attack,defense,hp){
 createPokemon(abilities, name, attack, defense, hp);
  }
 }
-  }
+
   xhr.open("GET", "https://pokeapi.co/api/v2/pokemon/vaporeon/", true);
   xhr.send();
 
@@ -116,9 +95,9 @@ createPokemon(abilities, name, attack, defense, hp);
 
 var xhp = new XMLHttpRequest();
 
-      function display3(){
   xhp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
+console.log("getting called");
 
     var r = xhp.responseText;
     var x = JSON.parse(r);
@@ -131,19 +110,7 @@ var xhp = new XMLHttpRequest();
      var hp = x.stats[5].base_stat;
 
 
-var i = 0;
-typeWriter();
 
-
-
-function typeWriter(){
-  if (i < name.length) {
-    document.getElementById("poke-name").innerHTML += name.charAt(i);
-    i++;
-    setTimeout(typeWriter, 100);
-  }
-
-}
 
 function createPokemon(abilities,name,attack,defense,hp){
 
@@ -162,7 +129,7 @@ function createPokemon(abilities,name,attack,defense,hp){
 createPokemon(abilities, name, attack, defense, hp);
  }
 }
-  }
+
 
   xhp.open("GET", "https://pokeapi.co/api/v2/pokemon/jolteon/", true);
 
@@ -201,8 +168,68 @@ class Trainer{
 
 
 
+  function display(){
+var button = document.getElementById("poke-name");
+var a = document.getElementById("hp");
+var b = document.getElementById("attack");
+var c = document.getElementById("defense");
+var d = document.getElementById("abilities");
+var e = document.getElementById("abilities2");
+button.innerHTML = flareon.name;
+ a.innerHTML = flareon.hp;
+ b.innerHTML = flareon.attack;
+ c.innerHTML = flareon.defense;
 
 
+d.innerHTML = flareon.abilities[0].ability.name;
+e.innerHTML = flareon.abilities[1].ability.name;
+
+
+
+  }
+
+
+  function display2(){
+var button = document.getElementById("poke-name");
+
+button.innerHTML = vaporeon.name;
+var a = document.getElementById("hp");
+var b = document.getElementById("attack");
+var c = document.getElementById("defense");
+var d = document.getElementById("abilities");
+var e = document.getElementById("abilities2");
+button.innerHTML = vaporeon.name;
+ a.innerHTML = vaporeon.hp;
+ b.innerHTML = vaporeon.attack;
+ c.innerHTML = vaporeon.defense;
+
+
+d.innerHTML = vaporeon.abilities[0].ability.name;
+e.innerHTML = vaporeon.abilities[1].ability.name;
+
+
+  }
+
+
+  function display3(){
+var button = document.getElementById("poke-name");
+
+button.innerHTML = jolteon.name;
+var a = document.getElementById("hp");
+var b = document.getElementById("attack");
+var c = document.getElementById("defense");
+var d = document.getElementById("abilities");
+var e = document.getElementById("abilities2");
+button.innerHTML = jolteon.name;
+ a.innerHTML = jolteon.hp;
+ b.innerHTML = jolteon.attack;
+ c.innerHTML = jolteon.defense;
+
+
+d.innerHTML = jolteon.abilities[0].ability.name;
+e.innerHTML = jolteon.abilities[1].ability.name;
+
+  }
 
 
 
@@ -251,6 +278,9 @@ count +=1;
   var avatar = document.getElementById("avatar1");
     avatar1.addEventListener("click", function(){
    avatar1.classList.toggle("flash");
+   var b = document.getElementById("pokemon").innerHTML
+     = '<img src =\'https://www.freeiconspng.com/uploads/flareon-pokemon-png-1.png\'> </img>';
+
 
   })
 
@@ -258,6 +288,10 @@ count +=1;
   var avatar2 = document.getElementById("avatar2");
     avatar2.addEventListener("click", function(){
    avatar2.classList.toggle("flash");
+   var b = document.getElementById("pokemon").innerHTML
+
+    ='<img src =\'https://vignette.wikia.nocookie.net/wingsoffire/images/1/1e/Jolteon.png/revision/latest?cb=20160624090722\'> </img>';
+
 
   })
 
@@ -265,5 +299,8 @@ count +=1;
   var avatar3 = document.getElementById("avatar3");
     avatar3.addEventListener("click", function(){
    avatar3.classList.toggle("flash");
+   var b = document.getElementById("pokemon").innerHTML
+
+    ='<img src =\'https://www.freeiconspng.com/uploads/vaporeon-free-png-3.png\'> </img>';
 
   })
